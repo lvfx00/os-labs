8c1d4f472ac69126bac102c95c3213b3c81dca7c
lvfx00 <lvfx00@gmail.com>
2018-03-24 13:12:02 +0000

    fixed 7th lab

--- lab7/lab7.c
+++ lab7/lab7.c
@@ -1,59 +1,81 @@
 #include <sys/types.h>
-#include <stdio.h>
 #include <fcntl.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <signal.h>
+#include <stdio.h>
 #include <string.h>
 #include <sys/mman.h>
 
+#define MAX_LEN 100
+
+volatile sig_atomic_t fd;
+
+void print_file() {
+    char ch;
+    lseek(fd, 0L, SEEK_SET);
+    int size = lseek(fd, 0, SEEK_END);
+    char *p = mmap(0, size, PROT_READ, MAP_SHARED, fd, 0);
+    for(int i = 0; i < size; ++i) {
+        printf("%c", p[i]);
+    }
+    // close program
+    exit(0);
+}
+
 int main(int argc, char *argv[])
 {
-    static char *displ[501];
-    char *p, buf[10];
-    int fd1, fd2, count, i = 1, j = 1, line_no, line_ln[500];
-    static char err_msg[32] = "Input file - ";
-    off_t size;
+    static struct sigaction sa;
 
-    if ((fd1 = open("/dev/tty", O_RDONLY | O_NDELAY)) == -1) {
-        perror("/dev/tty");
+    sa.sa_handler = print_file;
+    sigaction(SIGALRM, &sa, NULL);
+
+    if(( fd =  open(argv[1], O_RDONLY)) == -1) {
+        perror(argv[1]);
         exit(1);
     }
 
-    if ((fd2 = open(argv[1], O_RDONLY)) == -1) {
-        perror(strcat(err_msg, argv[1]));
-        exit(1);
-        }
+    char *offsets[MAX_LEN + 1];
+    int line_length[MAX_LEN + 1];
+    char c, buf[257];
 
-    size = lseek(fd2, 0, SEEK_END);
-    p = mmap(0, size, PROT_READ, MAP_SHARED, fd2, 0);
 
-    displ[1] = p;
-    for(count = 0; count < size; count++)
+    int size = lseek(fd, 0, SEEK_END);
+    char *p = mmap(0, size, PROT_READ, MAP_SHARED, fd, 0);
+
+    int i = 1; // iterator for curr string num
+    int j = 1; // iterator for number of symbols in curr string
+
+    // offsets array contains a pointers to the line beginnings
+    offsets[1] = p;
+    for(int count = 0; count < size; count++) {
         if( *(p+count) == '\n' ) {
-            line_ln[i++] = j;
-            displ[i] = count+p+1;
+            line_length[i++] = j;
+            offsets[i] = count + p + 1;
             j = 1;
-            }
-        else
-            j++;
-
-    displ[i] = 0;
-    for (;;) {
-        printf("you have 5 seconds to enter a line number\n");
-        sleep(5);
-        if ((i = read(fd1, buf, 10)) == 0) {
-            write(1, p, size);
-            exit(0);
         }
         else {
-            buf[i] = '\0';
-            line_no = atoi(buf);
-            if(line_no <= 0)
-                exit(0);
-            if(displ[line_no] != 0)
-                write(0, displ[line_no], line_ln[line_no]);
-            else
-                fprintf(stderr, "Bad Line Number\n");
+            j++;
         }
     }
+    // if last string not ends by '\n' it won't be printed (like in cat)
+    int number_of_lines = i - 1;
+
+    while (1) {
+        printf("Line number: ");
+        alarm(5);
+
+        int line_num;
+        if(scanf("%d", &line_num)) {
+            alarm(0);
+            if(line_num <= 0 || line_num > number_of_lines) {
+                exit(0);
+            }
+            write(0, offsets[line_num], line_length[line_num]);
+        } else {
+            exit(0);
+        }
+    }
+
+    return 0;
 }
